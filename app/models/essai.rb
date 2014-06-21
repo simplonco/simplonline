cd667637739a4c78f99f104977bfd89352329bb1
jum-s <vincent.jumeaux@gmail.com>
2014-06-21 17:25:38 +0200

    inscrit le résultat du test dans Essai.statut

--- app/models/essai.rb
+++ app/models/essai.rb
@@ -1,13 +1,14 @@
 class Essai < ActiveRecord::Base
 belongs_to :exercice
-  
+
   def run
     require File.path('~/code/app/sensei/public/essais/' + self.content)
     
     if app(self.exercice.input) == self.exercice.expected_output
-      true
+      self.status = true
     else
-      false 
+      self.status = false 
     end
+    self.status
   end
 end
