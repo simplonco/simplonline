e346d4ca3bafb03f5100091aafab41a85f7a99b6
Yannick Francois <yannick.francois@free.fr>
2014-06-27 23:57:15 +0200

    Add foreman to manage Sidekiq,
    Add backoffice url/sidekiq to look at sidekiq jobs (minimalist
    interface)
    Clean some binding pry

--- app/workers/verificateur.rb
+++ app/workers/verificateur.rb
@@ -3,8 +3,9 @@ class Verificateur
 
   def perform(essai_id)
     essai = Essai.find(essai_id)
+
     essai_contenu = File.open("#{Rails.root}/public#{essai.fichier_url}", "r").read
-    binding.pry
+
     output = `sudo docker run litaio/ruby /bin/echo -n "#{essai_contenu}" > test && bin/bash`
     #ruby -I. -e "require 'test'; app(#{essai.exercice.input})`
     if output == essai.exercice.expected_output
