085a7162a2bd7cd773d4e438762cc5291f720f7c
emak <emilien.ak@gmail.com>
2014-07-31 14:55:48 +0200

    Ajout d'un rescue pour les navigateurs bizarres

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
     utilisateur = Utilisateur.find_by_provider_and_uid(auth[:provider], auth[:uid]) || Utilisateur.create_with_omniauth(auth)
     session[:utilisateur_id] = utilisateur.id
     redirect_to root_url, notice: "Connecté!"
+  rescue 
+    redirect_to root_url, notice: "Impossible de se connecter, vérifiez vos addons"
   end
 
   def destroy
