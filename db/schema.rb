52282c6e1c89ff18c36fe7fde87e26e800a03f8f
Yannick Francois <yannick.francois@free.fr>
2014-08-07 20:24:19 +0200

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807085520) do
+ActiveRecord::Schema.define(version: 20140807095700) do
 
   create_table "essais", force: true do |t|
     t.string   "fichier"
