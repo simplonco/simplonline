a0afb6477dc747f9a7afda233ee0c752f6189c3c
emak <emilien.ak@gmail.com>
2014-09-02 16:12:02 +0200

    Set redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://localhost:6379/12', :namespace => 'sensei' }
+    config.redis = { :url => 'redis://redistogo:811942f2ce762ccc3225e3b85cc5b27a@jack.redistogo.com:10144/', :namespace => 'sensei' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://localhost:6379/12', :namespace => 'sensei' }
+    config.redis = { :url => 'redis://redistogo:811942f2ce762ccc3225e3b85cc5b27a@jack.redistogo.com:10144/', :namespace => 'sensei' }
 end
