5a4d2e75240bcaac9d3f4207633fc5a4379a8e13
emak <emilien.ak@gmail.com>
2014-09-02 18:13:13 +0200

    Clean for deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: bundle exec rails server -p $PORT -e $RAILS_ENV
 worker: bundle exec sidekiq
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-    config.redis = { :url => 'redis://redistogo:811942f2ce762ccc3225e3b85cc5b27a@jack.redistogo.com:10144/', :namespace => 'sensei' }
+    config.redis = { :url => ENV["REDIS_URL"], :namespace => 'sensei' }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { :url => 'redis://redistogo:811942f2ce762ccc3225e3b85cc5b27a@jack.redistogo.com:10144/', :namespace => 'sensei' }
+    config.redis = { :url => ENV["REDIS_URL"], :namespace => 'sensei' }
 end
