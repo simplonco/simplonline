b409f313f173b1b8767bb02301d24ab037f6e027
Yannick Francois <yannick.francois@free.fr>
2014-09-03 08:43:09 +0200

    Fix rename error

--- db/migrate/20140827122851_change_utilisateur_to_user.rb
+++ db/migrate/20140827122851_change_utilisateur_to_user.rb
@@ -1,6 +1,5 @@
 class ChangeUtilisateurToUser < ActiveRecord::Migration
   def change
     rename_column(:essais, :utilisateur_id, :user_id)
-    rename_index(:essais, :index_essais_on_utilisateur_id, :index_essais_on_user_id)
   end
 end
