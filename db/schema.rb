fb472f1ad858c674a250250c28abc603333dce48
Yannick Francois <yannick.francois@free.fr>
2014-09-17 19:20:12 +0200

    Add descrpition to lesson

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915083911) do
+ActiveRecord::Schema.define(version: 20140917171637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20140915083911) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "publish_at"
+    t.text     "description"
   end
 
   create_table "qcm_authors", force: true do |t|
