9baadd43d1aafd7acfbe9d99aa5c19245a588158
Yannick Francois <yannick.francois@free.fr>
2014-09-19 23:50:40 +0200

    Ajout des modifications manquantes pour le lexique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918065657) do
+ActiveRecord::Schema.define(version: 20140919102402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,14 @@ ActiveRecord::Schema.define(version: 20140918065657) do
     t.datetime "updated_at"
     t.datetime "publish_at"
     t.text     "description"
+  end
+
+  create_table "lexiques", force: true do |t|
+    t.string   "keyword"
+    t.text     "description"
+    t.text     "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "qcm_authors", force: true do |t|
