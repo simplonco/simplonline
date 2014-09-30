4db9156b28993e40fffc41622268b872351c39fa
emak <emilien.ak@gmail.com>
2014-09-30 10:24:05 +0200

    Ajoute et supprime les liens vers le lexique
     en markdown dans le content de chapter plutôt qu'en html

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -6,14 +6,16 @@ class Definition < ActiveRecord::Base
 
   def add_links_in_chapters
     Chapter.all.each do |chap|
-      chap.content = chap.content.gsub(/\b#{self.keyword.to_sym}\b/i, "<a href='/definitions/#{self.id}' class='definition'>#{self.keyword}</a>")
+      chap.content = chap.content.gsub(/[\*]*#{self.keyword.to_sym}[\*]*/i) { |s| "[#{s}](/definitions/#{self.id})" }
       chap.save!
     end
   end
 
   def delete_links_in_chapters
     Chapter.all.each do |chap|
-      chap.content = chap.content.gsub(/<a\s[^>]*>.*?#{self.keyword}.*?<\/a>/i, self.keyword)
+      chap.content = chap.content.gsub(/\[[\*]*#{self.keyword.to_sym}[\*]*]\(\/definitions\/#{self.id}\)/i) do |s|
+        s.match(/[\*]*#{self.keyword}[\*]*/i)
+      end
       chap.save!
     end
   end
--- test/models/definition_test.rb
+++ test/models/definition_test.rb
@@ -18,6 +18,22 @@ class DefinitionTest < ActiveSupport::TestCase
     definition = FactoryGirl.create(:definition)
     assert_equal [definition], Definition.last_updated
   end
+
+  test "insert definition in chapters" do
+    definition = FactoryGirl.create(:definition, keyword: 'ippon')
+    chapter = FactoryGirl.create(:chapter, content: "Le jambon c'est n'**ippon** ni mauvais")
+    definition.add_links_in_chapters
+    expected_content = "Le jambon c'est n'[**ippon**](/definitions/#{definition.id}) ni mauvais"
+    assert_equal expected_content, Chapter.first.content
+  end
+
+  test "remove definition in chapters" do
+    definition = FactoryGirl.create(:definition, keyword: 'ippon')
+    chapter = FactoryGirl.create(:chapter, content: "Le jambon c'est n'[**ippon**](/definitions/#{definition.id}) ni mauvais")
+    definition.delete_links_in_chapters
+    expected_content = "Le jambon c'est n'**ippon** ni mauvais"
+    assert_equal expected_content, Chapter.last.content
+  end
 end
 
 
