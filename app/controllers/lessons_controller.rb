08c7c0a2d53147c8dbab14f5ba1cb7997a2c09fd
jum-s <vincent.jumeaux@gmail.com>
2014-10-03 12:15:12 +0200

    add next_lesson link in lesson show

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -32,6 +32,7 @@ class LessonsController < ApplicationController
 
   def show
     @lesson = Lesson.find(params[:id])
+    @lessons = Lesson.online
     if current_user.remote? && @lesson.offline?
       flash[:error] = t('.not_found')
       redirect_to root_path
