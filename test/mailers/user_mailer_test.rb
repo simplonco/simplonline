cce3a2fd82f57c7ed00fc5509dc9a423661bc173
Yannick Francois <yannick.francois@free.fr>
2014-10-03 17:05:53 +0200

    Test that mail use foad@simplon.co

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,7 +7,7 @@ class UserMailerTest < ActionMailer::TestCase
     email = UserMailer.reset_password(user).deliver
     assert_not ActionMailer::Base.deliveries.empty?
 
-    assert_equal ['from@example.com'], email.from
+    assert_equal ['foad@simplon.co'], email.from
     assert_equal [user.email], email.to
     assert_equal I18n.t('user_mailer.reset_password.subject'), email.subject
     assert email.body.include?(user.name)
