f54caced2054b4c53ea15dea4536119e4f9d1b88
Yannick Francois <yannick.francois@free.fr>
2014-10-10 23:23:41 +0200

    Fix some stuff

--- db/migrate/20141010120514_add_column_for_parent_message.rb
+++ db/migrate/20141010120514_add_column_for_parent_message.rb
@@ -9,18 +9,24 @@ class AddColumnForParentMessage < ActiveRecord::Migration
     belongs_to :message
   end
 
-  def change
+  def up
     add_belongs_to :messages, :parent, class: 'Message'
 
     Comment.all.each do |comment|
-      new_message = Message.new
-      new_message.parent = comment.message
-      new_message.user = comment.user
-      new_message.created_at = comment.created_at
-      new_message.updated_at = comment.updated_at
-      new_message.content = comment.content
-      new_message.status = comment.status
-      new_message.save!
+      unless Message.find_by(content: comment.content)
+        new_message = Message.new
+        new_message.parent = comment.message
+        new_message.user = comment.user
+        new_message.created_at = comment.created_at
+        new_message.updated_at = comment.updated_at
+        new_message.content = comment.content
+        new_message.status = comment.status
+        new_message.save!
+      end
     end
+  end
+
+  def down
+    remove_belongs_to :messages, :parent
   end
 end
