8b9bff6f924e41c53d79876c81141ce7d1b89d67
Yannick Francois <yannick.francois@free.fr>
2014-11-06 23:15:00 +0100

    Specify to use simplonline-prod

--- bin/create_user.rb
+++ bin/create_user.rb
@@ -4,7 +4,7 @@ emails = File.open(file, 'r').readlines.map(&:chomp)
 
 emails.each do |mail|
   puts "work on #{mail}"
-  `appsdeck run rake simplon:create_remote email=#{mail}`
+  `appsdeck -a simplonline-prod run rake simplon:create_remote email=#{mail}`
 end
 
 
